d973a95a724d9e6402f5d5276e03552809dd3f79
Longzham1023 <trannamlong203@gmail.com>
2023-10-13 18:46:20 +0700

    Setup MVC and create Models: Author, Book,  CartIems, Category

---
+++ Program.cs
@@ -0,0 +1,69 @@
+﻿using BookStore.Data;
+using BookStore.Services;
+using BookStore.IServices;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+using Swashbuckle.AspNetCore.Filters;
+using Microsoft.OpenApi.Models;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+builder.Services.Configure<DatabaseSetting>(builder.Configuration.GetSection("MyDatabase"));
+
+//Resolving categoryService dependency
+builder.Services.AddTransient<ICategoryService, CategoryService>();
+builder.Services.AddTransient<IAuthorService, AuthorService>();
+builder.Services.AddTransient<IBookService, BookService>();
+builder.Services.AddTransient<IPublisherService, PublisherService>();
+builder.Services.AddTransient<IAuthService, AuthService>();
+builder.Services.AddTransient<ICustomerService, CustomerService>();
+
+// use authenticate
+
+
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(options =>
+    {
+        options.TokenValidationParameters = new TokenValidationParameters
+        {
+            ValidateIssuerSigningKey = true,
+            IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration.GetSection("Application:Token").Value!)),
+            ValidateIssuer = false,
+            ValidateAudience = false,
+        };
+    });
+
+var app = builder.Build();
+
+//Use CORS
+app.UseCors(builder => builder
+    .WithOrigins("https://localhost:7105") // Replace with your allowed origins
+    .AllowAnyHeader()
+    .AllowAnyMethod());
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthentication();
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
+
